1a1294837632d229d501fbc9612d210be4395119
ahmed <ahmed.belhajhasan@esprit.tn>
2021-12-06 00:54:24 +0100

    ajouter la version finale de ma tache+arduino

---
+++ rfid/mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "arduino.h"
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void update_label();   // slot permettant la mise à jour du label état de la lampe 1,
+    // ce slot est lancé à chaque réception d'un message de Arduino
+
+
+
+private:
+    Ui::MainWindow *ui;
+
+    QByteArray data; // variable contenant les données reçues
+
+    Arduino A; // objet temporaire
+};
+
+#endif // MAINWINDOW_H
