60e37c39669d3529c6032c6e4ce95e9a85f127b7
ahmed <ahmed.belhajhasan@esprit.tn>
2021-12-06 00:56:06 +0100

    ajouter la version finale de ma tache+arduino.ino

---
+++ rfid.ino
@@ -0,0 +1,107 @@
+
+//Viral Science
+//RFID
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Servo.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+#define LED_G 4 //define green LED pin
+#define LED_R 5
+//#define LED_R_qt 2//define red LED
+#define BUZZER 2 //buzzer pin
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+Servo myServo; //define servo name
+char data; // variable contenant le caractère lu
+
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  myServo.attach(3); //servo pin
+  myServo.write(0); //servo start position
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+ // pinMode(LED_R_qt, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  noTone(BUZZER);
+  Serial.println("Put your card to the reader...");
+  Serial.println();
+
+}
+void loop() 
+{
+  
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+
+  
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  
+  if (content.substring(1) == "03 EB 26 17") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    
+    digitalWrite(LED_G, HIGH);
+    delay(2000);
+    digitalWrite(LED_G, LOW);
+          Serial.write('1'); //envoyer le nouvel état de la lampe 
+          delay(2000);
+          Serial.write('3');
+          
+
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+
+    digitalWrite(LED_R, HIGH);
+    delay(1000);
+    digitalWrite(LED_R, LOW);
+                  Serial.write('0'); //envoyer le nouvel état de la lampe  
+
+     if (Serial.available()){ 
+                 data=Serial.read(); 
+                 if(data=='2')
+                 {
+                     tone(BUZZER,2000);
+    delay(1000);
+    noTone(BUZZER);
+    data='5';
+    
+             
+                
+                 }
+
+           }
+  }
+   delay(2000);
+          Serial.write('3');
+  
+} 
